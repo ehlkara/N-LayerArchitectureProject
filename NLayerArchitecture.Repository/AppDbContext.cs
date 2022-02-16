099784f0aa6b5a879e087e550d123658bc1c99b5
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-02-16 10:55:43 +0300

    added appDbContext

---
+++ NLayerArchitecture.Repository/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerArchitecture.Core;
+using NLayerArchitecture.Repository.Configurations;
+using System.Reflection;
+
+namespace NLayerArchitecture.Repository
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            // Real world example
+            //var p = new Product() { ProductFeature = new ProductFeature() };
+        }
+
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<ProductFeature>? ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            // Tek tek configrationları okuma yöntemi
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
