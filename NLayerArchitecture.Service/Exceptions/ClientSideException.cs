daf9124081dd8a6e5895fe0a01664859f56513f2
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-02-18 19:40:39 +0300

    added globalexceptionhandler

---
+++ NLayerArchitecture.Service/Exceptions/ClientSideException.cs
@@ -0,0 +1,10 @@
+﻿namespace NLayerArchitecture.Service.Exceptions
+{
+    public class ClientSideException:Exception
+    {
+        public ClientSideException(string message):base(message)
+        {
+
+        }
+    }
+}
