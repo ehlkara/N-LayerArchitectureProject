01d362736f135baaad2cd94b4bf0058b374976fb
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-02-21 16:02:43 +0300

    added CategoryApiService

--- NLayerArchitecture.Web/Services/CategoryApiService.cs
+++ NLayerArchitecture.Web/Services/CategoryApiService.cs
@@ -1,4 +1,6 @@
-﻿namespace NLayerArchitecture.Web.Services
+﻿using NLayerArchitecture.Core.DTOs;
+
+namespace NLayerArchitecture.Web.Services
 {
     public class CategoryApiService
     {
@@ -7,6 +9,12 @@
         public CategoryApiService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+        }
+
+        public async Task<List<CategoryDto>> GetAllAsync()
+        {
+            var response = await _httpClient.GetFromJsonAsync<CustomResponseDto<List<CategoryDto>>>("categories");
+            return response.Data;
         }
     }
 }
